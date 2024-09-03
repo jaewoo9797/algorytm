65f7165a79c4a829e2a91b4a3155062c5d97aaee
백재우 <9712jw@gmail.com>
2024-09-03 22:32:43 +0900

    알고리즘 08(유효한 팰린드롬)

---
+++ src/String08/Main.java
@@ -0,0 +1,31 @@
+package String08;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public String solution(String str) {
+        char[] arr = str.toUpperCase().toCharArray();
+        int lt=0, rt= arr.length-1;
+        while(lt<rt) {
+            if(!Character.isLetter(arr[lt])) {  //lt가 가리키는 곳이 문자열이 아닐 경우
+                lt++;
+            } else if(!Character.isLetter(arr[rt])) {   //rt가 가리키는 곳이 문자열이 아닐 경우
+                rt--;
+            } else {    //팰린드롬 문자열이 아닐 경우
+                if(arr[lt] != arr[rt]) return "NO";
+                lt++;
+                rt--;
+            }
+        }
+
+        return "YES";
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(T.solution(s));
+    }
+}
